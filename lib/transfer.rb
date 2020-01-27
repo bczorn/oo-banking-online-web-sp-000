835efba1add79ae4ab74d49d211dde087342986f
Becky Zorn <bczorn@gmail.com>
2020-01-27 17:30:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,23 @@
 class Transfer
-  attr_accessor
+  attr_accessor :sender, :receiver, :t_amt, :status
   
-  def initialize(transfer)
+  def initialize(sender, receiver, t_amt)
+    @sender = sender
+    @receiver = receiver
+    @t_amt = t_amt
+    @status = "pending"
+  end
+  
+  def valid?
+    
+  end
+  
+  
+  def execute_transation
+    
+  end
+  
+  def reverse_transfer
     
   end
   
